d6258ba7eff9d065a29737e9badf3958b43b0fe5
Jorge Rodriguez <jorge.rodriguez@essence-grp.com>
2020-12-02 14:29:26 +0100

    Adding Angular material and some path @alias

---
+++ src/app/core/core/core.module.ts
@@ -0,0 +1,27 @@
+import { NgModule, Optional, SkipSelf } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { environment } from '@env/environment';
+
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
+
+import { MAT_DATE_LOCALE } from '@angular/material/core';
+import { AngularMaterialModule } from '@angular-material/angular-mmaterial.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    AngularMaterialModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    RouterModule
+  ],
+  providers: [
+    { provide: MAT_DATE_LOCALE, useValue: environment.defaultLanguage }
+  ],
+})
+export class CoreModule { }
